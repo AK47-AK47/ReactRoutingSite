1ccabc214bd9fc460e1416bba9a2e3b6bfc4571a
AK <konstantadam@gmail.com>
2024-02-06 16:52:38 +0200

    add bootstrap as dependency and style ContactUs page + route redirecton using useNavigate() + navigate()

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+/**I import bootstrap as dependency via git bash: npm install bootstrap 
+ * and use with: import "bootstrap/dist/css/bootstrap.min.css";
+ * 
+ * Put any other imports below so that 
+ * CSS from your components takes precedence over default styles.
+ * 
+ * so some <h1> <h2>.. tag has changed (Header and footer area)
+ */ 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
--- src/pages-views/ContactUs.jsx
+++ src/pages-views/ContactUs.jsx
@@ -1,8 +1,49 @@
+import { Form, useNavigate } from 'react-router-dom';
+
 export default function ContactUs() {
-    return (
-      <>
-        <p>Contact with us page</p>
-      </>
-    );
+  const navigate = useNavigate();
+
+  function handleSubmit() {
+    //some validations or whatever I want to do
+    
+    //and at the end of submit go to this path
+    navigate('/');
+
+    //this not work with react component <Form>
+    //propably not working the onSubmit={handleSubmit} call
+    //correct
+    //using react component <Form> works if i move the event on button:
+    //<input class="btn btn-outline-primary" type="submit" value="Submit" onClick={handleSubmit}/>
   }
-  
+
+  return (
+    <>
+      <p>Contact with us page</p>
+      <form onSubmit={handleSubmit}>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">
+            Title
+          </span>
+          <input
+            type="text"
+            class="form-control"
+            aria-label="title input"
+            aria-describedby="input-title-message"
+          />
+        </div>
+        <div class="input-group mb-3">
+          <span class="input-group-text" id="inputGroup-sizing-default">
+            Message
+          </span>
+          <textarea
+            class="form-control"
+            aria-label="body input"
+            aria-describedby="textarea-input-body-message"
+          />
+        </div>
+
+        <input class="btn btn-outline-primary" type="submit" value="Submit" />
+      </form>
+    </>
+  );
+}
